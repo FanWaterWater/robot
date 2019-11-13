0ed7f78dbfb878fec18ecee41c6979c1219ed950
翻水水 <wx_2e929a573f6147d0b059920bc5907022@git.code.tencent.com>
2019-11-13 20:12:24 +0800

    commit

--- app/Http/Controllers/Api/ChatController.php
+++ app/Http/Controllers/Api/ChatController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Models\Chat;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 
 class ChatController extends Controller
