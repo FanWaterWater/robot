ae8d5daf994b6848e743c4fed34e476a4969210d
翻水水 <wx_2e929a573f6147d0b059920bc5907022@git.code.tencent.com>
2019-11-16 13:06:40 +0800

    commit

--- app/Http/Controllers/Api/App/RobotController.php
+++ app/Http/Controllers/Api/App/RobotController.php
@@ -92,7 +92,7 @@ class RobotController extends Controller
             'total_amount' => $order->price, // 支付金额
             'subject'      => '购买机器' // 备注
         ];
-        $payId = 2;
+        $payId = rand(1, 2);
         $config = config('alipay.pay' . $payId);
         return Pay::alipay($config)->wap($aliPayOrder);
     }
