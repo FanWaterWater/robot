2a362a04b8f620abfbb5e1d147b6064bdd40384a
翻水水 <wx_2e929a573f6147d0b059920bc5907022@git.code.tencent.com>
2019-11-18 15:00:02 +0800

    commit

--- app/Http/Controllers/Api/Backend/UserController.php
+++ app/Http/Controllers/Api/Backend/UserController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api\Backend;
 
 use App\Models\User;
+use App\Utils\FundType;
+use App\Models\UserFund;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Cache;
