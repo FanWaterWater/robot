b92384c5b892fd82cf4ead177ee7831224ba3794
翻水水 <wx_2e929a573f6147d0b059920bc5907022@git.code.tencent.com>
2019-11-23 11:33:06 +0800

    commit

--- app/Http/Controllers/Api/Backend/AdminController.php
+++ app/Http/Controllers/Api/Backend/AdminController.php
@@ -11,7 +11,7 @@ class AdminController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('superadmin')->except(['index']);
+        $this->middleware('superadmin')->except(['index', 'login', 'logout']);
     }
 
     public function index()
