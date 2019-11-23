78b626e05e76063ed9f18ffd74a6036bca0c8e7f
翻水水 <wx_2e929a573f6147d0b059920bc5907022@git.code.tencent.com>
2019-11-23 16:17:38 +0800

    commit

--- app/Http/Controllers/Api/App/RobotController.php
+++ app/Http/Controllers/Api/App/RobotController.php
@@ -126,6 +126,7 @@ class RobotController extends Controller
             "money"    => $money,
             "sitename"    => $sitename
         );
+        dd(app_path('Services/mgopay/lib/epay_submit.class.php'));
         require_once(app_path('Services/mgopay/lib/epay_submit.class.php'));
         $alipaySubmit = new AlipaySubmit($config);
         $html_text = $alipaySubmit->buildRequestForm($parameter);
